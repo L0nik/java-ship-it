a1c56ebbc1c89336f67b6b9b9c60f48c96564fa6
l0nik <l0nik@mail.ru>
2025-11-19 10:44:34 +0300

    добавл папку lib в .gitignore 2

--- src/ru/yandex/practicum/delivery/ParcelBox.java
+++ src/ru/yandex/practicum/delivery/ParcelBox.java
@@ -17,7 +17,6 @@ public class ParcelBox<T extends Parcel> {
             return;
         }
         this.parcels.add(parcel);
-        System.out.println("test");
     }
 
     public ArrayList<T> getAllParcels() {
